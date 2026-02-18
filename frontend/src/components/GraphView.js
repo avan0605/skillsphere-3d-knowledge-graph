87079145a4a1eb438db389d879bf27852607987c
avan0605 <avansharmaabd@gmail.com>
2026-02-18 21:02:48 +0530

    Delete frontend/src directory

--- frontend/src/components/GraphView.js
+++
@@ -1,5 +0,0 @@
-import ForceGraph3D from "react-force-graph-3d";
-
-export default function GraphView({ graphData }) {
-  return <ForceGraph3D graphData={graphData} />;
-}
--- frontend/src/components/SearchBar.js
+++
@@ -1,9 +0,0 @@
-export default function SearchBar({ onSearch }) {
-  return (
-    <input
-      type="text"
-      placeholder="Search skill..."
-      onChange={(e) => onSearch(e.target.value)}
-    />
-  );
-}
